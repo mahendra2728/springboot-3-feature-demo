3df231ea2a6af0f01f1237cf97a4a90dde068f7e
mahendra <mahendramaske28@gmail.com>
2023-11-08 17:09:50 +0530

    first commit

---
+++ src/main/java/com/pm/products/ProductServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pm.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceDemoApplication.class, args);
+	}
+
+}
